118fa8b191b5750c5f8cd0f2abe6b4acd5ac9d79
chao <chao.uiuc@gmail.com>
2017-07-15 19:25:22 -0500

    script

--- scripts/tweets-10k.sh
+++ scripts/tweets-10k.sh
@@ -4,8 +4,8 @@ config='tweets-10k.yaml'
 # python ../code/preprocess.py $config
 
 # Run
-export MKL_NUM_THREADS=2
-export OMP_NUM_THREADS=2
+export MKL_NUM_THREADS=4
+export OMP_NUM_THREADS=4
 python ../code/main.py $config
 # python ../code/main_neg.py $config
 
