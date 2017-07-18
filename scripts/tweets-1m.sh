0af32cee802d18b08ca896812f208d4788e4120e
chao <chao.uiuc@gmail.com>
2017-07-18 12:36:59 -0500

    script

--- scripts/tweets-1m.sh
+++ scripts/tweets-1m.sh
@@ -1,12 +1,13 @@
 config='tweets-1m.yaml'
 
 # Preprocessing data
-python ../code/preprocess.py $config
+# python ../code/preprocess.py $config
 
 # Run
-export OMP_NUM_THREADS=12
-export MKL_NUM_THREADS=12
+export OMP_NUM_THREADS=15
+export MKL_NUM_THREADS=15
 python ../code/main.py $config
+# python ../code/model_neg.py $config
 
 # Postprocesing results
 
